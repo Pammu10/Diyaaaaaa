4edf1677c295487fd61794564d66079e21980d62
Pramodh Krishna <96466131+Pammu10@users.noreply.github.com>
2022-08-21 13:35:34 +0530

    Add files via upload

--- problem2.c
+++ problem2.c
@@ -1,4 +1,5 @@
-//Pramodh Krishna
+/*
+Pramodh Krishna
 #include<stdio.h>
 #define wsize 10
 
@@ -31,5 +32,62 @@ void push(char c){
 }
 
 char pop(){
+    return s.a[s.top--];
+}
+*/
+
+//Pramodh Krishna
+#include<stdio.h>
+#include<string.h>
+#define wsize 8
+
+struct stack{
+    int top; 
+    char* a[wsize];
+}s, ns;
+
+void push_s(char*);
+char* pop();
+void display();
+
+int main()
+{
+    int n, i, k;
+    char w[wsize];
+    char nw[wsize];
+    char *ptr;
+    char *srt;
+    gets(w);
+    s.top = -1;
+    k = 0;
+    ptr = w;
+    srt = nw;
+    for (ptr = w; *ptr != '\0'; ptr++){
+        srt = ptr;
+        printf("%c   ", *srt);
+        push_s(srt);
+    }
+    //display();
+    for (i = 0; i < strlen(w); i++){
+        printf("%d", s.top);
+        printf("%s\n", pop());
+    }
+        
+    //for (i = 0; i < n; i++)
+    //    printf("%s", pop());
+    //return 0;
+}
+
+void display(){
+    int temp = s.top;
+    while (temp > -1){
+        printf("%s\n", s.a[temp--]);
+    }
+
+}
+void push_s(char *c){
+    s.a[++s.top]= c;
+}
+char* pop(){
     return s.a[s.top--];
 }
